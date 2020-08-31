3f0b5c8e47968ef5322d40b681074ab662b48ee2
marco <marco.hitschler@spelzenhof.com>
2020-08-31 20:06:34 +0200

    eMail Notifications Update

--- szh_update.php
+++ szh_update.php
@@ -19,4 +19,27 @@ add_filter( 'allow_major_auto_core_updates', '__return_true' );
 add_filter( 'auto_update_plugin', '__return_true' );
 add_filter( 'auto_update_theme', '__return_true' );
 
+/**
+ * Disable eMail Notifications
+ * 
+ * Core, Plugins, Themes
+ *
+ * @since 0.1
+ */
+
+// Disable core auto-update email notifications.
+function szh_stop_update_emails( $send, $type, $core_update, $result ) {
+    if ( ! empty( $type ) && $type == 'success' ) {
+    return false;
+    }
+    return true;
+}
+add_filter( 'auto_core_update_send_email', 'szh_stop_update_emails', 10, 4 );
+    
+// Disable plugins auto-update email notifications.
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+     
+// Disable themes auto-update email notifications.
+add_filter( 'auto_theme_update_send_email', '__return_false' );
+
 ?>
